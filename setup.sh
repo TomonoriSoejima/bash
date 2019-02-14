93e733fb338f67cfb532430e9d970a8728cd4ef3
TomonoriSoejima <25199092+TomonoriSoejima@users.noreply.github.com>
2019-02-14 16:44:34 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,26 @@
+echo running lab setup script for $1
+
+echo curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$1.tar.gz
+curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$1.tar.gz
+
+echo curl -L -O https://artifacts.elastic.co/downloads/kibana/kibana-$1-darwin-x86_64.tar.gz
+curl -L -O https://artifacts.elastic.co/downloads/kibana/kibana-$1-darwin-x86_64.tar.gz
+
+
+mkdir $1
+mv elasticsearch-$1.tar.gz kibana-$1-darwin-x86_64.tar.gz $1
+
+cd $1
+echo working in $(pwd)
+tar xzvf elasticsearch-$1.tar.gz
+mv elasticsearch-$1 elasticsearch
+tar xzvf kibana-$1-darwin-x86_64.tar.gz
+mv kibana-$1-darwin-x86_64 kibana
+
+echo disabling security by adding xpack.security.enabled: false to ymls
+
+echo "xpack.security.enabled: false" >> elasticsearch/config/elasticsearch.yml
+echo "xpack.security.enabled: false" >> kibana/config/kibana.yml
+
+echo making a start up script
+
