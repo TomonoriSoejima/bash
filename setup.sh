4bca391d2768f28979e025a027a9450dfe9156af
TomonoriSoejima <25199092+TomonoriSoejima@users.noreply.github.com>
2020-04-12 09:39:39 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,35 @@
 echo running lab setup script for $1
 
-echo curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$1.tar.gz
-curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$1.tar.gz
+# 7.x https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.6.0-darwin-x86_64.tar.gz
+
+elasticsearch=""
+if [ $(echo $1 | cut -c1) -le 6 ];
+then
+	elasticsearch=elasticsearch-$1.tar.gz
+else
+  echo it is 7
+  	elasticsearch=elasticsearch-$1-darwin-x86_64.tar.gz
+
+ fi
+
+
+# 6.x https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.8.1.tar.gz
+echo curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/$elasticsearch
+curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/$elasticsearch
+
 
 echo curl -L -O https://artifacts.elastic.co/downloads/kibana/kibana-$1-darwin-x86_64.tar.gz
 curl -L -O https://artifacts.elastic.co/downloads/kibana/kibana-$1-darwin-x86_64.tar.gz
 
 
 mkdir $1
-mv elasticsearch-$1.tar.gz kibana-$1-darwin-x86_64.tar.gz $1
+mv $elasticsearch kibana-$1-darwin-x86_64.tar.gz $1
 
+
+while sleep
 cd $1
 echo working in $(pwd)
-tar xzvf elasticsearch-$1.tar.gz
+tar xzvf $elasticsearch
 mv elasticsearch-$1 elasticsearch
 tar xzvf kibana-$1-darwin-x86_64.tar.gz
 mv kibana-$1-darwin-x86_64 kibana
@@ -21,6 +38,19 @@ echo disabling security by adding xpack.security.enabled: false to ymls
 
 echo "xpack.security.enabled: false" >> elasticsearch/config/elasticsearch.yml
 echo "xpack.security.enabled: false" >> kibana/config/kibana.yml
+echo "logging.dest: /Users/surfer/elastic/labs/$1/kibana/kibana.log" >> kibana/config/kibana.yml
 
+echo making a start up script
 
+echo "./elasticsearch/bin/elasticsearch -d" >> start.sh
+echo "./kibana/bin/kibana &" >> start.sh
 
+./start.sh
+
+sleep 15
+
+echo applying license file
+cred="elastic:changeme"
+curl -XPUT -u $cred 'http://localhost:9200/_xpack/license' -H "Content-Type: application/json" -d @/Users/surfer/elastic/tools/license.json
+
+echo you may get the internal license from https://wiki.elastic.co/display/PM/Internal+License+-+X-Pack
